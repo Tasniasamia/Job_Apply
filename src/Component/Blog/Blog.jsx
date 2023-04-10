757d5caf07e00f820a1b0d763e63e94cd7e822b8
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-04-11 01:28:04 +0600

    blog

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -2,8 +2,59 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className='container my-5'>
+           <div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+      When should you use context API?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+      <div class="accordion-body">
+     We have to use context API.because We can easily get data from children using the context API. Children can also easily access data from children inside children.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+      What is a custom hook?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+      <div class="accordion-body">
+      Hooks are functions that we use to load data or set data. Custom hooks are hooks that we create ourselves.we can build logic and use it different times.we can use it different times by custom hook easily.
+
+
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+      What is useRef?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+      <div class="accordion-body">
+       useRef is a function.we use it for different installation and dom manupulation.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingfour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapsefour" aria-expanded="false" aria-controls="panelsStayOpen-collapsefour">
+      What is useMemo?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapsefour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingfour">
+      <div class="accordion-body">
+      we don't recalculate by any value when we using usememo.It also helps to load application data easily.we we calculate any value.It renders this time.we have an expensive function that runs on every render. When changing the count or adding a todo, you will notice a delay in execution.
+      </div>
+    </div>
+  </div>
+</div> 
         </div>
     );
 };
